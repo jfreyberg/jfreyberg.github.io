71f79160cdba0ad0382142ce48c8616e98f72dc3
Julian Freyberg <julian_freyberg@live.de>
2023-04-04 18:42:25 +0200

    minor bugfixes

---
+++ _app/immutable/chunks/2.1e0b84c5.js
@@ -0,0 +1,6 @@
+import { _ } from "./_page.9aeaa6fb.js";
+import { default as default2 } from "../entry/_page.svelte.4e566e98.js";
+export {
+  default2 as component,
+  _ as universal
+};
---
+++ _app/immutable/chunks/3.1c4490f1.js
@@ -0,0 +1,6 @@
+import { _ } from "./_page.f330256e.js";
+import { default as default2 } from "../entry/plain-page.svelte.19e56192.js";
+export {
+  default2 as component,
+  _ as universal
+};
